004ac89473862351c67bbb78bbc99662fcf755bf
s-m-9 <maga0030@algonquinlive.com>
2018-11-14 12:17:18 -0500

    committing development-branch files

---
+++ app/src/main/java/bmm/com/threeinone/ChooseActivity.kt
@@ -0,0 +1,12 @@
+package bmm.com.threeinone
+
+import android.app.Activity
+import android.os.Bundle
+
+class ChooseActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose)
+    }
+}
